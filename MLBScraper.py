795f27bc3f16a092065c89b43b4781cad5351c5c
AATreok <dougtrippe@gmail.com>
2016-09-26 20:15:24 -0600

    work in progress

--- MLBScraper.py
+++ MLBScraper.py
@@ -2,13 +2,14 @@ import urllib.request
 import urllib.error
 from bs4 import BeautifulSoup
 import datetime
+import time.sleep
 import sys
 
 
 def getGameStatus(team):
     team_abr = team
     usehomedata = False
-    useawaydata = True
+    useawaydata = False
     team_home = False
     now = datetime.datetime.now()
     urlyear = 'year_' + str(now.year)
@@ -52,33 +53,84 @@ def getGameStatus(team):
                 print('Game is finalized!')
                 if int(game_info['home_team_runs']) > int(game_info['away_team_runs']):
                     print('THE ' + game_info['home_team_name'].upper() + ' WON!')
-                    print('Final score: ' + game_info['home_team_name'] + ' ' + game_info['home_team_runs'] + ' ' + game_info[
-                        'away_team_name'] + ' ' +
+                    print('Final score: ' + game_info['home_team_name'] + ' ' + game_info['home_team_runs'] + ' ' +
+                          game_info[
+                              'away_team_name'] + ' ' +
                           game_info['away_team_runs'])
                     return 1
                 else:
                     print('The ' + game_info['home_team_name'] + ' lost :( [home]...')
-                    print('Final score: ' + game_info['home_team_name'] + ' ' + game_info['home_team_runs'] + ' ' + game_info[
-                        'away_team_name'] + ' ' +
+                    print('Final score: ' + game_info['home_team_name'] + ' ' + game_info['home_team_runs'] + ' ' +
+                          game_info[
+                              'away_team_name'] + ' ' +
                           game_info['away_team_runs'])
                     return 2
             else:
                 print('Game is finalized!')
                 if int(game_info['away_team_runs']) > int(game_info['home_team_runs']):
                     print('THE ' + game_info['away_team_name'].upper() + ' WON!')
-                    print('Final score: ' + game_info['away_team_name'] + ' ' + game_info['away_team_runs'] + ' ' + game_info[
-                        'home_team_name'] + ' ' +
+                    print('Final score: ' + game_info['away_team_name'] + ' ' + game_info['away_team_runs'] + ' ' +
+                          game_info[
+                              'home_team_name'] + ' ' +
                           game_info['home_team_runs'])
                     return 1
                 else:
                     print('The ' + game_info['away_team_name'] + ' lost :( [away]...')
-                    print('Final score: ' + game_info['away_team_name'] + ' ' + game_info['away_team_runs'] + ' ' + game_info[
-                        'home_team_name'] + ' ' +
+                    print('Final score: ' + game_info['away_team_name'] + ' ' + game_info['away_team_runs'] + ' ' +
+                          game_info[
+                              'home_team_name'] + ' ' +
                           game_info['home_team_runs'])
                     return 2
+        else:
+            print("Game hasn't started or in progress (but exists).")
+            return 4
+
+
+def nextDay(freezetime):
+    safeiter = 0
+    while (freezetime.day < datetime.datetime.now().day()):
+        safeiter += 1
+        time.sleep(3600)
+        if safeiter >= 23:
+            break
+
+
+def netError(freezetime):
+    time.sleep(60)
+
+
+def sendProbe():
+    print('There is a URL error.')
+
+
+def makePost():
+    print("Create karma post.")
 
 
 if __name__ == '__main__':
-    if getGameStatus('ana') == -1:
-        print("Error getting URL")
-    sys.exit()
+    # -1 --> error, check for multiple errors and send notice if problem persists for over 1>hr
+    # 0 --> no game, wait until next day
+    # 1 --> Red Sox won (home or away)
+    # 2 --> Red Sox lost, wait until next GAME (not day, in case doublheader)
+    # 4 --> continue, game in progress
+    thistime = datetime.datetime.now()
+    # starts the bot; runs indefinitely
+    netsafe = 0
+    while True:
+        gamestatus = getGameStatus('ana')
+        while gamestatus != 1:
+            if gamestatus == -1:
+                if netsafe >= 5:
+                    sendProbe()
+                    sys.exit()
+                netsafe += 1
+                netError(thistime)
+            if gamestatus == 0:
+                nextDay(thistime)
+            if gamestatus == 1:
+                makePost(thistime)
+            if gamestatus == 2:
+                makePost(thistime)
+            if gamestatus == 4:
+                # 60 second pause, then recheck
+                time.sleep(60)
